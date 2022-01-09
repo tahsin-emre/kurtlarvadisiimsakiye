abc66365338c3e4b1afcfa098d8c58f16ac30a0e
Tahsin Emre <telli@tahsinemre.com>
2022-01-10 00:51:59 +0300

    end

--- lib/services/sectionservice.dart
+++ lib/services/sectionservice.dart
@@ -2,102 +2,169 @@ import 'package:kurtlarvadisiimsakiye/models/sectionmodel.dart';
 
 class SectionService {
   static List<SectionModel> sectionList = [
-    SectionModel.fromList(1, 1, ''),
-    SectionModel.fromList(2, 1, ''),
-    SectionModel.fromList(3, 1, ''),
-    SectionModel.fromList(4, 2, ''),
-    SectionModel.fromList(5, 2, ''),
-    SectionModel.fromList(6, 2, ''),
-    SectionModel.fromList(7, 3, ''),
-    SectionModel.fromList(8, 3, ''),
-    SectionModel.fromList(9, 3, ''),
-    SectionModel.fromList(10, 4, ''),
-    SectionModel.fromList(11, 4, ''),
-    SectionModel.fromList(12, 4, ''),
-    SectionModel.fromList(13, 4, ''),
-    SectionModel.fromList(14, 5, ''),
-    SectionModel.fromList(15, 5, ''),
-    SectionModel.fromList(16, 5, ''),
-    SectionModel.fromList(17, 5, ''),
-    SectionModel.fromList(18, 6, ''),
-    SectionModel.fromList(19, 6, ''),
-    SectionModel.fromList(20, 6, ''),
-    SectionModel.fromList(21, 6, ''),
-    SectionModel.fromList(22, 7, ''),
-    SectionModel.fromList(23, 7, ''),
-    SectionModel.fromList(24, 7, ''),
-    SectionModel.fromList(25, 8, ''),
-    SectionModel.fromList(26, 8, ''),
-    SectionModel.fromList(27, 8, ''),
-    SectionModel.fromList(28, 9, ''),
-    SectionModel.fromList(29, 9, ''),
-    SectionModel.fromList(30, 9, ''),
-    SectionModel.fromList(31, 10, ''),
-    SectionModel.fromList(32, 10, ''),
-    SectionModel.fromList(33, 10, ''),
-    SectionModel.fromList(34, 11, ''),
-    SectionModel.fromList(35, 11, ''),
-    SectionModel.fromList(36, 11, ''),
-    SectionModel.fromList(37, 11, ''),
-    SectionModel.fromList(38, 12, ''),
-    SectionModel.fromList(39, 12, ''),
-    SectionModel.fromList(40, 12, ''),
-    SectionModel.fromList(41, 12, ''),
-    SectionModel.fromList(42, 13, ''),
-    SectionModel.fromList(43, 13, ''),
-    SectionModel.fromList(44, 13, ''),
-    SectionModel.fromList(45, 13, ''),
-    SectionModel.fromList(46, 14, ''),
-    SectionModel.fromList(47, 14, ''),
-    SectionModel.fromList(48, 14, ''),
-    SectionModel.fromList(49, 15, ''),
-    SectionModel.fromList(50, 15, ''),
-    SectionModel.fromList(51, 15, ''),
-    SectionModel.fromList(52, 16, ''),
-    SectionModel.fromList(53, 16, ''),
-    SectionModel.fromList(54, 16, ''),
-    SectionModel.fromList(55, 17, ''),
-    SectionModel.fromList(56, 17, ''),
-    SectionModel.fromList(57, 17, ''),
-    SectionModel.fromList(58, 18, ''),
-    SectionModel.fromList(59, 18, ''),
-    SectionModel.fromList(60, 18, ''),
-    SectionModel.fromList(61, 18, ''),
-    SectionModel.fromList(62, 19, ''),
-    SectionModel.fromList(63, 19, ''),
-    SectionModel.fromList(64, 19, ''),
-    SectionModel.fromList(65, 19, ''),
-    SectionModel.fromList(66, 20, ''),
-    SectionModel.fromList(67, 20, ''),
-    SectionModel.fromList(68, 20, ''),
-    SectionModel.fromList(69, 20, ''),
-    SectionModel.fromList(70, 21, ''),
-    SectionModel.fromList(71, 21, ''),
-    SectionModel.fromList(72, 21, ''),
-    SectionModel.fromList(73, 22, ''),
-    SectionModel.fromList(74, 22, ''),
-    SectionModel.fromList(75, 22, ''),
-    SectionModel.fromList(76, 23, ''),
-    SectionModel.fromList(77, 23, ''),
-    SectionModel.fromList(78, 23, ''),
-    SectionModel.fromList(79, 24, ''),
-    SectionModel.fromList(80, 24, ''),
-    SectionModel.fromList(81, 24, ''),
-    SectionModel.fromList(82, 25, ''),
-    SectionModel.fromList(83, 25, ''),
-    SectionModel.fromList(84, 25, ''),
-    SectionModel.fromList(85, 25, ''),
-    SectionModel.fromList(86, 27, ''),
-    SectionModel.fromList(87, 27, ''),
-    SectionModel.fromList(88, 27, ''),
-    SectionModel.fromList(89, 27, ''),
-    SectionModel.fromList(90, 28, ''),
-    SectionModel.fromList(91, 28, ''),
-    SectionModel.fromList(92, 28, ''),
-    SectionModel.fromList(93, 29, ''),
-    SectionModel.fromList(94, 29, ''),
-    SectionModel.fromList(95, 29, ''),
-    SectionModel.fromList(96, 29, ''),
-    SectionModel.fromList(97, 30, ''),
+    SectionModel.fromList(1, 1, 'https://www.youtube.com/watch?v=wg1KMFNkm8g'),
+    SectionModel.fromList(2, 1, 'https://www.youtube.com/watch?v=rkLNMxP4cPk'),
+    SectionModel.fromList(3, 1, 'https://www.youtube.com/watch?v=sLZjCY81nVg'),
+    SectionModel.fromList(4, 2, 'https://www.youtube.com/watch?v=xNsUwABdVgo'),
+    SectionModel.fromList(5, 2, 'https://www.youtube.com/watch?v=snh1q3hPqbs'),
+    SectionModel.fromList(6, 2, 'https://www.youtube.com/watch?v=6EW9l82Xl0o'),
+    SectionModel.fromList(7, 3, 'https://www.youtube.com/watch?v=RQfK4-xF_OU'),
+    SectionModel.fromList(8, 3, 'https://www.youtube.com/watch?v=OqiKIEX8msw'),
+    SectionModel.fromList(9, 3, 'https://www.youtube.com/watch?v=CggIIIUaug0'),
+    SectionModel.fromList(10, 4, 'https://www.youtube.com/watch?v=YM9x0HWXV3c'),
+    SectionModel.fromList(11, 4, 'https://www.youtube.com/watch?v=wLkx4hNi-V8'),
+    SectionModel.fromList(12, 4, 'https://www.youtube.com/watch?v=i8m2bHllZLQ'),
+    SectionModel.fromList(13, 4, 'https://www.youtube.com/watch?v=lHXGVdZFT3o'),
+    SectionModel.fromList(14, 5, 'https://www.youtube.com/watch?v=_bxpH4zRRnY'),
+    SectionModel.fromList(15, 5, 'https://www.youtube.com/watch?v=axO5t7I20-k'),
+    SectionModel.fromList(16, 5, 'https://www.youtube.com/watch?v=eSmQEACVzh8'),
+    SectionModel.fromList(17, 5, 'https://www.youtube.com/watch?v=rNI2sn8O0zU'),
+    SectionModel.fromList(18, 6, 'https://www.youtube.com/watch?v=tF2EnVbbwLo'),
+    SectionModel.fromList(19, 6, 'https://www.youtube.com/watch?v=bhE17LUCLSg'),
+    SectionModel.fromList(20, 6, 'https://www.youtube.com/watch?v=ryiBDwGrbxE'),
+    SectionModel.fromList(21, 6, 'https://www.youtube.com/watch?v=HzoNCsKHy9A'),
+    SectionModel.fromList(22, 7, 'https://www.youtube.com/watch?v=8NAnoD_iq-8'),
+    SectionModel.fromList(23, 7, 'https://www.youtube.com/watch?v=BROy37vJ6q4'),
+    SectionModel.fromList(24, 7, 'https://www.youtube.com/watch?v=tP7gNVs0WWI'),
+    SectionModel.fromList(25, 8, 'https://www.youtube.com/watch?v=1XzpMWaAC9g'),
+    SectionModel.fromList(26, 8, 'https://www.youtube.com/watch?v=K7e8GAevacc'),
+    SectionModel.fromList(27, 8, 'https://www.youtube.com/watch?v=g2lV3ufHLp4'),
+    SectionModel.fromList(28, 9, 'https://www.youtube.com/watch?v=re0t3gaDZlE'),
+    SectionModel.fromList(29, 9, 'https://www.youtube.com/watch?v=Buq-X_LNy4A'),
+    SectionModel.fromList(30, 9, 'https://www.youtube.com/watch?v=V_zfNjN32f4'),
+    SectionModel.fromList(
+        31, 10, 'https://www.youtube.com/watch?v=JHlLeWpE9Gs'),
+    SectionModel.fromList(
+        32, 10, 'https://www.youtube.com/watch?v=eWufxLsUW5c'),
+    SectionModel.fromList(
+        33, 10, 'https://www.youtube.com/watch?v=-G7e4kiDNqs'),
+    SectionModel.fromList(
+        34, 11, 'https://www.youtube.com/watch?v=L6tkE4Ibty8'),
+    SectionModel.fromList(
+        35, 11, 'https://www.youtube.com/watch?v=e8TaK-sbiiQ'),
+    SectionModel.fromList(
+        36, 11, 'https://www.youtube.com/watch?v=EIokbQs5SJQ'),
+    SectionModel.fromList(
+        37, 11, 'https://www.youtube.com/watch?v=MVuGQFLTNuU'),
+    SectionModel.fromList(
+        38, 12, 'https://www.youtube.com/watch?v=7Bi2CZhpERc'),
+    SectionModel.fromList(
+        39, 12, 'https://www.youtube.com/watch?v=0uPa_iWBTx0'),
+    SectionModel.fromList(
+        40, 12, 'https://www.youtube.com/watch?v=FNTghiX3IN4'),
+    SectionModel.fromList(
+        41, 12, 'https://www.youtube.com/watch?v=lmwOtXOxuao'),
+    SectionModel.fromList(
+        42, 13, 'https://www.youtube.com/watch?v=MiW4gBTLods'),
+    SectionModel.fromList(
+        43, 13, 'https://www.youtube.com/watch?v=U4Ix1HAkgEw'),
+    SectionModel.fromList(
+        44, 13, 'https://www.youtube.com/watch?v=oFHwRIGDM4Y'),
+    SectionModel.fromList(
+        45, 13, 'https://www.youtube.com/watch?v=8OozZA83NCo'),
+    SectionModel.fromList(
+        46, 14, 'https://www.youtube.com/watch?v=jpHPKFIXd0U'),
+    SectionModel.fromList(
+        47, 14, 'https://www.youtube.com/watch?v=7bnPgrg47Dc'),
+    SectionModel.fromList(
+        48, 14, 'https://www.youtube.com/watch?v=FCT59jtnKo4'),
+    SectionModel.fromList(
+        49, 15, 'https://www.youtube.com/watch?v=5WmdnPicUoU'),
+    SectionModel.fromList(
+        50, 15, 'https://www.youtube.com/watch?v=DIKiMMHDFLs'),
+    SectionModel.fromList(
+        51, 15, 'https://www.youtube.com/watch?v=lEz_g46E4oc'),
+    SectionModel.fromList(
+        52, 16, 'https://www.youtube.com/watch?v=X0WPNsfEork'),
+    SectionModel.fromList(
+        53, 16, 'https://www.youtube.com/watch?v=h2aRMwUGSd4'),
+    SectionModel.fromList(
+        54, 16, 'https://www.youtube.com/watch?v=scoTYBO3PQk'),
+    SectionModel.fromList(
+        55, 17, 'https://www.youtube.com/watch?v=f9F5XJf312Q'),
+    SectionModel.fromList(
+        56, 17, 'https://www.youtube.com/watch?v=MdUagydhgQQ'),
+    SectionModel.fromList(
+        57, 17, 'https://www.youtube.com/watch?v=FFycUjjyXMY'),
+    SectionModel.fromList(
+        58, 18, 'https://www.youtube.com/watch?v=HSRxTn_rzSw'),
+    SectionModel.fromList(
+        59, 18, 'https://www.youtube.com/watch?v=QXRPetUXPGQ'),
+    SectionModel.fromList(
+        60, 18, 'https://www.youtube.com/watch?v=CDo0s9jbCwU'),
+    SectionModel.fromList(
+        61, 18, 'https://www.youtube.com/watch?v=ZSeSWBNJKZg'),
+    SectionModel.fromList(
+        62, 19, 'https://www.youtube.com/watch?v=xwEHKKJMy28'),
+    SectionModel.fromList(
+        63, 19, 'https://www.youtube.com/watch?v=FdONLxBrubM'),
+    SectionModel.fromList(
+        64, 19, 'https://www.youtube.com/watch?v=cFLhwdDn3MU'),
+    SectionModel.fromList(
+        65, 19, 'https://www.youtube.com/watch?v=_XvX1j1Cfu8'),
+    SectionModel.fromList(
+        66, 20, 'https://www.youtube.com/watch?v=OhZ5WK33ar4'),
+    SectionModel.fromList(
+        67, 20, 'https://www.youtube.com/watch?v=rr7hOm2tsEY'),
+    SectionModel.fromList(
+        68, 20, 'https://www.youtube.com/watch?v=CW4FHwT-fAE'),
+    SectionModel.fromList(
+        69, 20, 'https://www.youtube.com/watch?v=FHO5MTPSILI'),
+    SectionModel.fromList(
+        70, 21, 'https://www.youtube.com/watch?v=0R2hSA2SPDk'),
+    SectionModel.fromList(
+        71, 21, 'https://www.youtube.com/watch?v=xblWpvB2Vnk'),
+    SectionModel.fromList(
+        72, 21, 'https://www.youtube.com/watch?v=wxyTtM1c7SY'),
+    SectionModel.fromList(
+        73, 22, 'https://www.youtube.com/watch?v=vVkZvixZrxI'),
+    SectionModel.fromList(
+        74, 22, 'https://www.youtube.com/watch?v=RYDk2bXOI4s'),
+    SectionModel.fromList(
+        75, 22, 'https://www.youtube.com/watch?v=_sbVg1AmNRU'),
+    SectionModel.fromList(
+        76, 23, 'https://www.youtube.com/watch?v=5GlRT2baGQ4'),
+    SectionModel.fromList(
+        77, 23, 'https://www.youtube.com/watch?v=LBuPaHJNXto'),
+    SectionModel.fromList(
+        78, 23, 'https://www.youtube.com/watch?v=cD_QRQ006yE'),
+    SectionModel.fromList(
+        79, 24, 'https://www.youtube.com/watch?v=hfgdV11HZGo'),
+    SectionModel.fromList(
+        80, 24, 'https://www.youtube.com/watch?v=sXMYSbSsFrU'),
+    SectionModel.fromList(
+        81, 24, 'https://www.youtube.com/watch?v=I_B45sQnN60'),
+    SectionModel.fromList(
+        82, 25, 'https://www.youtube.com/watch?v=mlvCi209PbQ'),
+    SectionModel.fromList(
+        83, 25, 'https://www.youtube.com/watch?v=LLCUO0lJVDY'),
+    SectionModel.fromList(
+        84, 25, 'https://www.youtube.com/watch?v=ALUm2DN0bnc'),
+    SectionModel.fromList(
+        85, 25, 'https://www.youtube.com/watch?v=200UZDMQn4A'),
+    SectionModel.fromList(
+        86, 27, 'https://www.youtube.com/watch?v=MXErUsiS1X8'),
+    SectionModel.fromList(
+        87, 27, 'https://www.youtube.com/watch?v=J33H-dqZKIs'),
+    SectionModel.fromList(
+        88, 27, 'https://www.youtube.com/watch?v=gLWabLBnwYw'),
+    SectionModel.fromList(
+        89, 27, 'https://www.youtube.com/watch?v=YPyV8F2kYfQ'),
+    SectionModel.fromList(
+        90, 28, 'https://www.youtube.com/watch?v=wB3h7Q9AH00'),
+    SectionModel.fromList(
+        91, 28, 'https://www.youtube.com/watch?v=eDJ84QNxP2A'),
+    SectionModel.fromList(
+        92, 28, 'https://www.youtube.com/watch?v=sjIOQ9lFLW4'),
+    SectionModel.fromList(
+        93, 29, 'https://www.youtube.com/watch?v=1hXoKJUigEU'),
+    SectionModel.fromList(
+        94, 29, 'https://www.youtube.com/watch?v=_hv7Frs2ZZ4'),
+    SectionModel.fromList(
+        95, 29, 'https://www.youtube.com/watch?v=zi-Cjqi_tLk'),
+    SectionModel.fromList(
+        96, 29, 'https://www.youtube.com/watch?v=qX7VHQ56fYE'),
+    SectionModel.fromList(
+        97, 30, 'https://www.youtube.com/watch?v=io4CGNb27Xk'),
   ];
 }
--- lib/views/homescreen.dart
+++ lib/views/homescreen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:kurtlarvadisiimsakiye/models/sectionmodel.dart';
+import 'package:kurtlarvadisiimsakiye/models/daymodel.dart';
+import 'package:kurtlarvadisiimsakiye/services/dayservice.dart';
+import 'package:kurtlarvadisiimsakiye/services/sectionservice.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -21,7 +24,36 @@ class _HomeScreenState extends State<HomeScreen> {
   AppBar get buildAppBar {
     return AppBar(
       title: const Text('2022 Kurtlar Vadisi İmsakiyesi'),
+      actions: [
+        InkWell(
+          onTap: () => showInfo(),
+          child: const Icon(Icons.info_outline),
+        )
+      ],
     );
+  }
+
+  void showInfo() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Uygulama Hakkında'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text(
+                    'Bu uygulama Tahsin Emre Telli tarafından 2022 yılı ramazan ayına ithafen yapılmıştır.'),
+              ],
+            ),
+            actions: [
+              ElevatedButton(
+                onPressed: () => launch('https:tahsinemre.com'),
+                child: const Text('tahsinemre.com'),
+              )
+            ],
+          );
+        });
   }
 
   Widget get buildBody {
@@ -41,14 +73,52 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget dayList() {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...DayService.dayList.map((e) => dayTile(e)),
+        ],
+      ),
+    );
   }
 
-  Widget sectionTile(SectionModel model) {
-    return Container();
+  Widget dayTile(DayModel model) {
+    String sections = '';
+    for (var item in model.sections) {
+      sections += item.toString() + ', ';
+    }
+    return ListTile(
+      tileColor:
+          model.day % 2 == 0 ? Colors.grey.shade100 : Colors.grey.shade300,
+      title: Text(model.day.toString() + ' Nisan'),
+      trailing: Text(sections != '' ? sections : 'Kadir Gecesi'),
+      onTap: () => showSections(model),
+    );
   }
 
-  Widget dayTile() {
-    return Container();
+  void showSections(DayModel m) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(m.day.toString() + ' Nisan Bölümleri'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ...SectionService.sectionList
+                    .where((element) => element.day == m.day)
+                    .map((e) => InkWell(
+                          onTap: () => launch(e.link),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(e.id.toString() + '. Bölüm'),
+                          ),
+                        ))
+              ],
+            ),
+          );
+        });
   }
 }
