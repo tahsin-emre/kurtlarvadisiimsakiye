495216314d83360196cac2c8d26a9fe6c0682b49
Tahsin Emre <telli@tahsinemre.com>
2022-01-09 22:45:35 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/tahsinemre/kurtlarvadisiimsakiye/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tahsinemre.kurtlarvadisiimsakiye
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homescreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar,
+      body: buildBody,
+    );
+  }
+
+  AppBar get buildAppBar {
+    return AppBar();
+  }
+
+  Widget get buildBody {
+    return Container();
+  }
+}
