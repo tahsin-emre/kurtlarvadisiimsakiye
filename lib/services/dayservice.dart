f0926e2835c2a7547385d1d19458902ecb37a913
Tahsin Emre <telli@tahsinemre.com>
2022-01-09 23:51:03 +0300

    daylist updated

--- lib/services/dayservice.dart
+++ lib/services/dayservice.dart
@@ -2,6 +2,35 @@ import 'package:kurtlarvadisiimsakiye/models/daymodel.dart';
 
 class DayService {
   static List<DayModel> dayList = [
-    DayModel.fromList(1, [1, 2, 3])
+    DayModel.fromList(1, [1, 2, 3]),
+    DayModel.fromList(2, [4, 5, 6]),
+    DayModel.fromList(3, [7, 8, 9]),
+    DayModel.fromList(4, [10, 11, 12, 13]),
+    DayModel.fromList(5, [14, 15, 16, 17]),
+    DayModel.fromList(6, [18, 19, 20, 21]),
+    DayModel.fromList(7, [22, 23, 24]),
+    DayModel.fromList(8, [25, 26, 27]),
+    DayModel.fromList(9, [28, 29, 30]),
+    DayModel.fromList(10, [31, 32, 33]),
+    DayModel.fromList(11, [34, 35, 36, 37]),
+    DayModel.fromList(12, [38, 39, 40, 41]),
+    DayModel.fromList(13, [42, 43, 44, 45]),
+    DayModel.fromList(14, [46, 47, 48]),
+    DayModel.fromList(15, [49, 50, 51]),
+    DayModel.fromList(16, [52, 53, 54]),
+    DayModel.fromList(17, [55, 56, 57]),
+    DayModel.fromList(18, [58, 59, 60, 61]),
+    DayModel.fromList(19, [62, 63, 64, 65]),
+    DayModel.fromList(20, [66, 67, 68, 69]),
+    DayModel.fromList(21, [70, 71, 72]),
+    DayModel.fromList(22, [73, 74, 75]),
+    DayModel.fromList(23, [76, 77, 78]),
+    DayModel.fromList(24, [79, 80, 81]),
+    DayModel.fromList(25, [82, 83, 84, 85]),
+    DayModel.fromList(26, []),
+    DayModel.fromList(27, [86, 87, 88, 89]),
+    DayModel.fromList(28, [90, 91, 92]),
+    DayModel.fromList(29, [93, 94, 95, 96]),
+    DayModel.fromList(30, [97]),
   ];
 }
